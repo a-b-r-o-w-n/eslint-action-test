80fbca7439fd33bf44263d3f23b51a7967a1578f
Andy Brown <asbrown002@gmail.com>
2019-12-26 18:16:16 -0600

    add eslint-plugin-import

--- packages/client-ts/.eslintrc.js
+++ packages/client-ts/.eslintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
   extends: 'eslint-config-react-app',
+  plugins: ['import'],
   rules: {
-    'eqeqeq': 'error'
+    'eqeqeq': 'error',
+
+    'import/exports-last': 'error',
+    'import/first': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-unused-modules': 'error',
+    'import/no-useless-path-segments': 'error',
+    'import/order': ['error', { 'newlines-between': 'always' }],
   }
 }
