3826a29c088f718920ce087e3ed33f331b975792
Andy Brown <asbrown002@gmail.com>
2021-04-16 08:49:26 -0700

    update format

--- test-echo.js
+++ test-echo.js
@@ -1,3 +1,3 @@
 console.log(
-  "::error file=packages/client-ts/src/App.tsx,line=13,col=21::Use lower case class name"
+  "::error file=packages/client-ts/src/App.tsx,line=8,col=11::[eqeqeq] Expected '===' and instead saw '=='"
 );
